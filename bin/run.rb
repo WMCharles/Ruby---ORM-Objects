37f22fb4a370f04625fdbb451ec279925200a514
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-31 13:33:31 +0300

    add create class method
    creates and saves data to db

--- bin/run.rb
+++ bin/run.rb
@@ -2,5 +2,7 @@ require_relative '../config/environment'
 
 Song.create_table
 
-love_is_weaked = Song.new(name:'Love is Weaked', album:'Brick && Lance')
-love_is_weaked.save  
+# love_is_weaked = Song.new(name:'Love is Weaked', album:'Brick && Lance')
+# love_is_weaked.save 
+
+Song.create('Mbagala', 'Diamond')
--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,10 @@ class Song
 
         self
     end
+
+    def self.create(name, album)
+        song = Song.new(name: name, album: album)
+        song.save 
+    end
+    
 end
